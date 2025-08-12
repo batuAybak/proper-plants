b5ee6c0be4d4ff45c66690c583f00209dcb95b6e
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-12 16:08:45 -0400

    add syling sheets into main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "../styling/cart.css";
+import "../styling/plants.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
