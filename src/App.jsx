f89f73de10d917a4ea961ef22beba766e413762b
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-12 16:09:43 -0400

    add app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import Cart from "../components/cart";
+import Plants from "../components/plants";
+import PLANTS from "./data";
+
 export default function App() {
-  return <></>;
+  let plantsArr = PLANTS.map((plant) => ({ ...plant, cartCount: 0 })); //add new key and value to tract count
+  const [cart, setCart] = useState(plantsArr);
+
+  //Function to increase the count of a plant in given plant id
+  function addToCart(id) {
+    setCart((cart) =>
+      cart.map((plant) =>
+        id === plant.id ? { ...plant, cartCount: plant.cartCount + 1 } : plant
+      )
+    );
+  }
+
+  //Function to decrease the count of a plant in given plant id
+  function removeFromCart(id) {
+    setCart((cart) =>
+      cart.map((plant) =>
+        id === plant.id ? { ...plant, cartCount: plant.cartCount - 1 } : plant
+      )
+    );
+  }
+
+  return (
+    <>
+      <h1>Proper Plants</h1>
+      <main>
+        <Plants cart={cart} addToCart={addToCart} />
+        <Cart
+          cart={cart}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      </main>
+    </>
+  );
 }
