6c81209c34598a2d8953fbb645547a249b997d4a
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-13 12:51:06 -0400

    wrap the context around main

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import "../styling/cart.css";
 import "../styling/plants.css";
+import { PlantProvider } from "./PlantContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <PlantProvider>
+      <App />
+    </PlantProvider>
   </StrictMode>
 );
