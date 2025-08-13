5b379887d36e522e3d93c9cabbe5998e93a9fce0
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-13 12:51:27 -0400

    add the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { useState } from "react";
 import Cart from "../components/cart";
 import Plants from "../components/plants";
-import PLANTS from "./data";
+import { usePlantContext } from "./PlantContext";
 
 export default function App() {
-  let plantsArr = PLANTS.map((plant) => ({ ...plant, cartCount: 0 })); //add new key and value to track count
-  const [cart, setCart] = useState(plantsArr);
-
-  //Function to increase the count of a plant in given plant id
-  function addToCart(id) {
-    setCart((cart) =>
-      cart.map((plant) =>
-        id === plant.id ? { ...plant, cartCount: plant.cartCount + 1 } : plant
-      )
-    );
-  }
-
-  //Function to decrease the count of a plant in given plant id
-  function removeFromCart(id) {
-    setCart((cart) =>
-      cart.map((plant) =>
-        id === plant.id ? { ...plant, cartCount: plant.cartCount - 1 } : plant
-      )
-    );
-  }
+  const { cart, addToCart, removeFromCart } = usePlantContext();
 
   return (
     <>
